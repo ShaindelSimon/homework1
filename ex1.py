ff29be10f1c6d99a6dfa73d99fce85cf35ca846b
ShaindelSimon <129065456+ShaindelSimon@users.noreply.github.com>
2024-02-06 19:57:03 +0200

    Add ex1

---
+++ ex1.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+m1 = np.matrix([
+	[1, 2],
+	[3, 4]
+])
+m2 = np.matrix([
+	[10, 20],
+	[30, 40]
+])
+
+print("m1:")
+print(m1)
+print("m2:")
+print(m2)
+print("m1+m2:")
+print(m1+m2)
+print("m1*m2:")
+print(m1*m2)
